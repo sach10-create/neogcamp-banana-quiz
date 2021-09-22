b65845a5532bd09df51e77ed92eede4a186c1773
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-23 00:44:57 +0530

    added the translator on console

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var button_action = document.querySelector("#btn");
+var buttonAction = document.querySelector("#translate-button");
+var userInput = document.querySelector("#translate-input");
+var output = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(text) {
+    return serverURL + "?" + "text=" + text
+}
 
 function clickHandler() {
-    console.log("clicked");
+    // console.log("clicked");
+    var user_input = userInput.value;
+
+    fetch(getTranslationUrl(user_input))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
 };
-button_action.addEventListener("click", clickHandler);
+
+buttonAction.addEventListener("click", clickHandler)
