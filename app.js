8390cc9159efadddd452a249412c745f0fce9fac
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-22 23:23:06 +0530

    added header

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("script test")
+var button_action  = document.querySelector("#btn");
+
+function clickHandler(){
+    console.log("clicked");
+};
+button_action.addEventListener("click", clickHandler);
